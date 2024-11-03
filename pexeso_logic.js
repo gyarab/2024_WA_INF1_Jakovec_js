8f7192543adaf50f91ad010a7f7fde2a6b98e62c
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2024-11-03 20:21:11 +0100

    text under beginning and new audio

--- pexeso_logic.js
+++ pexeso_logic.js
@@ -3,16 +3,18 @@ const container = document.querySelector(".game-container");
 let selectedCards = [];
 let player = 0;
 let pairsFound = 0;
+let cardsFound0 = 0;
+let cardsFound1 = 0;
 
 let cardsArray = [...images, ...images];
 
+/*generace*/ 
 for (let i = cardsArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     let temp = cardsArray[i];
     cardsArray[i] = cardsArray[j];
     cardsArray[j] = temp;
 }
-
 
 for (let i = 0; i < cardsArray.length; i++) {
     let cardElement = Instantiate(cardsArray[i]);
@@ -55,18 +57,25 @@ function Check() {
 
     if (card0.image === card1.image) {
         pairsFound += 1;
-        selectedCards.forEach(cardElement => {
-            cardElement.card.style.backgroundImage = "🗣️";
-        });
         selectedCards = [];
-        var success_a = new Audio('success.mp3');
-        success_a.play();
+        setTimeout(function() {
+            var success_a = new Audio('success.mp3');
+            success_a.play();
+        }, 300);
+        
+        if(player===0)cardsFound0+=1;
+        else if (player===1)cardsFound1+=1;
+        pairsDisplay.textContent = `Pairs Found: ${pairsFound}`;
+
         if (pairsFound*2 === cardsArray.length) {
             setTimeout(function() {
                 alert("You did it! All cards matched!");
             }, 500);
         }
     } else {
+        var wrong_a = new Audio('wrong.mp3');
+        wrong_a.volume = 0.4;
+        wrong_a.play();
         setTimeout(function() {
            FlipBack(card0);
             setTimeout(function() {
@@ -74,5 +83,34 @@ function Check() {
                 selectedCards = [];
             }, 50);
         }, 1000);
+        ChangePlayer();
     }
 }
+
+function ChangePlayer(){
+    if(player===0) player = 1;
+    else player = 0;
+    console.log(player);
+    /*highlight*/ 
+}
+
+/*skore print*/ 
+const statsContainer = document.createElement("div");
+statsContainer.className = "stats-container";
+container.parentNode.insertBefore(statsContainer, container.nextSibling);
+
+
+const player1Text = document.createElement("h1");
+player1Text.className = "player-text";
+player1Text.textContent = `Player 1`;
+statsContainer.appendChild(player1Text);
+
+const pairsDisplay = document.createElement("div");
+pairsDisplay.className = "pairs-display";
+pairsDisplay.textContent = `Pairs Found: ${pairsFound}`;
+statsContainer.appendChild(pairsDisplay);
+
+const player2Text = document.createElement("h1");
+player2Text.className = "player-text";
+player2Text.textContent = `Player 2`;
+statsContainer.appendChild(player2Text);
